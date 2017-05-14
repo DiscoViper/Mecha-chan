b2eea6b6d04c71735ff354429e5a8869e420ab1e
smardis <mechaidol@gmail.com>
2017-05-13 20:02:26 -0500

    mecha base
    Mecha-chan bot with some basic commands

---
+++ mecha.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const TOKEN = "MzEyNjM0NTM2OTA1NDA4NTIy.C_k5ug.KHFOP8gY193wy-kwo94guDTpICI";
+const PREFIX = ".>";
+
+const bot = new Discord.Client();
+
+bot.on("ready", function() {
+    console.log("Mecha online");
+});
+
+bot.on('guildMemberAdd', function(member) {
+    member.guild.channels.find("name", "general").sendMessage(member.toString() + "Welcome to the test server!");
+});
+
+bot.on("message", function(message) {
+    if(message.author.equals(bot.user))
+        return;
+
+    if(!message.content.startsWith(PREFIX))
+        return;
+
+    var args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0].toLowerCase()) {
+        case "ping":
+          message.reply("pong");
+          break;
+        case "avatar":
+          message.reply(message.author.avatarURL);
+          break;
+    }
+});
+
+bot.login(TOKEN);
